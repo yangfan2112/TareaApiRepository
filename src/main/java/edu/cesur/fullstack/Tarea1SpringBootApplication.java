7b8e78656b947ab4a9e1e8fd295b3f7cfb83d773
yangfanlu <ivanluyangfan21@gmail.com>
2023-10-12 17:44:54 +0200

    Creacion de los paquetes

---
+++ src/main/java/edu/cesur/fullstack/Tarea1SpringBootApplication.java
@@ -0,0 +1,13 @@
+package edu.cesur.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea1SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea1SpringBootApplication.class, args);
+	}
+
+}
